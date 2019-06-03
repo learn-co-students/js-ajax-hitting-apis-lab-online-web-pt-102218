ab19c93c8db4810017fade264d4ff9515b24523e
Arye Eidelman <aryedov.e@gmail.com>
2019-06-03 00:09:18 -0400

    Passing repo tests

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-// your code here
+function getRepositories() {
+  let req = new XMLHttpRequest();
+  let username = document.getElementById('username').value
+  req.addEventListener('load', displayRepositories);
+  req.open('GET', `https://api.github.com/users/${username}/repos`);
+  req.send();
+}
+
+function getCommits() {
+
+}
+
+function getBranches() {
+
+
+}
+function displayRepositories() {
+  let repos = JSON.parse(this.responseText);
+  let reposHtml = `<ul>${repos.map(r =>
+    `<li><a href="${r.html_url}">${r.owner.login}/${r.name}</a></li>`
+  ).join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = reposHtml;
+}
+
+function displayCommits() {
+
+}
+
+function displayBranches() {
+
+}
