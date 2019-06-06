ec6398e7839ec804878fea5b21d997fc3df3ce5f
Johnathon Wood <jaywood128@gmail.com>
2019-06-06 17:00:20 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // your code here
+function getRepositories() {
+  let user = document.getElementById('username').value
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayRepositories) 
+  req.open('GET', `https://api.github.com/users/${user}/repos`);
+  req.send();
+}
+
+function displayRepositories() {
+  debugger
+  var repos = JSON.parse(this.responseText); 
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(
+      r =>
+        '<li>' +
+        r.name +
+        ' - <a href="#" data-repo="' +
+        r.name +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
+function getCommits() {
+  
+}
